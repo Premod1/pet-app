530d439f78ae52af64e21504f7ff751a6fb7279c
premod1 <premodsuraweera1@gmail.com>
2023-05-20 23:29:15 +0530

    done

---
+++ src/components/Navbar.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { NButton } from 'naive-ui'
+
+</script>
+<template>
+    <h1>Navbar</h1>
+    <n-space>
+        <n-button type="primary">done</n-button>
+    </n-space>
+</template>
