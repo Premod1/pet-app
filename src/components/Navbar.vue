6163603ae7c4da9104bcf75c391e3c702cdb92a4
premod1 <premodsuraweera1@gmail.com>
2023-05-22 21:34:24 +0530

    done

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,11 @@
 <script setup>
-import { NButton } from 'naive-ui'
+
 
 </script>
 <template>
-    <h1>Navbar</h1>
-    <n-space>
-        <n-button type="primary">done</n-button>
-    </n-space>
+    <div class="flex items-center justify-between p-4 bg-gray-400">
+        <h1 class="pl-4">Navbar</h1>
+        <button class="w-20 h-10 font-black text-white bg-blue-800 rounded-lg hover:bg-blue-950">Login</button>
+    </div>
+    
 </template>
