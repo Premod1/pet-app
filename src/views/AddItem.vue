ef6367fb33d4ae6c544c7b9b5ed303143c14fc80
premod1 <premodsuraweera1@gmail.com>
2023-05-27 15:56:38 +0530

    file upload

--- src/views/AddItem.vue
+++ src/views/AddItem.vue
@@ -1,46 +1,99 @@
 <script setup>
-import console from 'console';
-import { ref } from 'vue';
+import { NInput } from "naive-ui";
+import {  reactive } from "vue";
+import { db } from "../firebase/index";
+import {
+  collection,
+  doc,
+  addDoc,
+  setDoc,
+  getStorage,
+  ref as storageRef, uploadBytes
+} from "firebase/firestore";
 
-const itemName = ref('')
-const itemPrice = ref('')
-const itemImage = ref('')
+const userData = reactive({
+  itemName: "",
+  itemPrice: "",
+  file: "",
+});
 
-function handleAdd() {
-  const itemDetails = {
-    itemName: itemName.value,
-    itemPrice: itemPrice.value,
-    itemImage: itemImage.value
+async function handleAdd() {
+  console.log(userData);
+  try {
+    const fileData = await readFileAsDataURL(userData.file);
+    const newItemData = {
+      itemName: userData.itemName,
+      itemPrice: userData.itemPrice,
+      file: fileData,
+    };
+    const docRef = await addDoc(collection(db, 'items'), newItemData);
+    console.log('added successfully', docRef);
+  } catch (error) {
+    console.log('error', error);
   }
-
-  console.log(itemDetails)
 }
 
+async function readFileAsDataURL(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+    reader.onerror = reject;
+    reader.readAsDataURL(file);
+  });
+}
+function handleFileChange(event) {
+  const file = event.target.files[0];
+  console.log(file);
+  userData.file = file;
+  
+  
+}
 </script>
 <template>
-    <div class="flex flex-col items-center justify-center gap-10">
-        <h1 class="text-2xl font-black">Add Item</h1>
-        <div class="bg-slate-300 w-[500px] h-[400px] p-10">
-            <h2>Enter Item Name</h2>
-            <n-input size="large" round placeholder="Enter Item Name" v-model="itemName" />
-            <h2>Enter Item Price</h2>
-            <n-input size="large" round placeholder="Enter Item Price" type="number" v-model="itemPrice"  />
-            <br/>
-            <br/>
-            <n-upload v-model="itemImage"
-    action="https://www.mocky.io/v2/5e4bafc63100007100d8b70f"
-    :headers="{
-      'naive-info': 'hello!'
-    }"
-    :data="{
-      'naive-data': 'cool! naive!'
-    }"
-  >
-    <n-button class="bg-blue-500">Upload File</n-button>
-  </n-upload>
-            <br/>
-            <br/>
-            <button class="w-full h-10 text-xl font-black text-white bg-green-700 " @click="handleAdd()">Add</button>
-        </div>
+  <div class="flex flex-col items-center justify-center gap-10">
+    <h1 class="text-2xl font-black">Add Item</h1>
+    <div class="bg-slate-300 w-[500px] h-[400px] p-10">
+      <h2>Enter Item Name</h2>
+      <n-input
+        size="large"
+        round
+        placeholder="Enter Item Name"
+        v-model:value="userData.itemName"
+      />
+      <h2>Enter Item Price</h2>
+      <n-input
+        size="large"
+        round
+        placeholder="Enter Item Price"
+        type="number"
+        v-model:value="userData.itemPrice"
+      />
+      <br />
+      <br />
+      <!-- <n-upload
+        action="https://www.mocky.io/v2/5e4bafc63100007100d8b70f"
+        :headers="{
+          'naive-info': 'hello!',
+        }"
+        :data="{
+          'naive-data': 'cool! naive!',
+        }"
+        
+      >
+        <n-button class="bg-blue-500">Upload File</n-button>
+      </n-upload> -->
+      <input type="file" v-on:change="handleFileChange" />
+
+      <br />
+      <br />
+      <button
+        class="w-full h-10 text-xl font-black text-white bg-green-700"
+        @click="handleAdd"
+      >
+        Add
+      </button>
     </div>
+  </div>
 </template>
