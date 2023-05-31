caf3e4d40901fd65da797e34a05572c72c8729d8
premod1 <premodsuraweera1@gmail.com>
2023-05-31 22:26:40 +0530

    about page done

---
+++ src/components/About.vue
@@ -0,0 +1,41 @@
+<script setup></script>
+<template>
+  <div class="grid grid-flow-col grid-cols-2 gap-4">
+    <div>
+      <img
+        src="https://images.unsplash.com/photo-1592194996308-7b43878e84a6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
+        alt=""
+        class="p-10"
+      />
+    </div>
+    <div>
+      <h2 class="pt-20 pr-10 text-lg text-center">
+        Welcome to Jack Pet Supplies and Sherlock Rescue! We are a unique retail
+        store that offers a wide range of high-quality pet products and
+        services, as well as support for the local animal rescue community.
+      </h2>
+      <p class="pt-20 w-[650px] text-lg leading-10">
+        Our store is run by a team of passionate and knowledgeable pet lovers
+        who are dedicated to providing exceptional customer service and expert
+        advice on all things related to pet care. We take pride in offering a
+        diverse selection of pet products, from premium dog food and treats to
+        toys, beds, and grooming supplies. Whether you're a first-time pet owner
+        or a seasoned pro, we have everything you need to keep your furry
+        friends happy, healthy, and well-cared for. <br/>In addition to our retail
+        offerings, we also support the local animal rescue community through our
+        partnership with Sherlock Rescue, a dedicated team of animal lovers who
+        are committed to rescuing and rehabilitating dogs in need. At Jack Pet
+        Supplies, we proudly display posters, host adoption events, and
+        contribute a percentage of the sales of certain products to the centre.
+        We believe in the importance of giving back to our community and making
+        a positive impact on the lives of animals in need. Our commitment to
+        exceptional customer service, high-quality pet products, and support for
+        the local animal rescue community sets us apart from other pet supply
+        stores. We invite you to visit us in person and experience the
+        difference for yourself. Thank you for choosing Jack Pet Supplies and
+        Sherlock Rescue, and for supporting our mission to make a difference in
+        the lives of animals in need.
+      </p>
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,12 +4,14 @@ import Navbar from "@/components/Navbar.vue";
 import Product from "@/components/Product.vue";
 import Subcribe from "@/components/Subcribe.vue";
 import Footer from "@/components/Footer.vue";
+import About from "@/components/About.vue";
 </script>
 <template>
   <div >
     <Navbar />
     <Banner />
     <Product/>
+    <About/>
     <Subcribe />
     <Footer />
     
